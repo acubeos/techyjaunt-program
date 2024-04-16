51e338ee6f7f416806667ee516b1b7779845d2f5
acubeos <80309463+acubeos@users.noreply.github.com>
2024-04-16 15:10:07 +0100

    assignment two

--- acubeos/classAssignment2.js
+++ acubeos/classAssignment2.js
@@ -19,12 +19,19 @@ class Transaction {
 }
 
 class BankAccount {
-	constructor(accountNumber, firstName, lastName, initialBalance = 0) {
+	constructor(
+		accountNumber,
+		firstName,
+		lastName,
+		initialBalance = 0,
+		dailyWithdrawalLimit = Infinity
+	) {
 		this._accountNumber = accountNumber
 		this._firstName = firstName
 		this._lastName = lastName
 		this._balance = initialBalance
 		this._transactions = []
+		this._dailyWithdrawalLimit = dailyWithdrawalLimit
 	}
 
 	get accountNumber() {
@@ -48,7 +55,18 @@ class BankAccount {
 	}
 
 	get transactions() {
-		return this._transactions
+		return this._transactions.map((transaction) => ({
+			type: transaction.type,
+			amount: transaction.amount,
+			timestamp: transaction.timestamp,
+		}))
+	}
+
+	get dailyWithdrawalLimit() {
+		return this._dailyWithdrawalLimit
+	}
+	set dailyWithdrawalLimit(newLimit) {
+		this._dailyWithdrawalLimit = newLimit
 	}
 
 	// Method to deposit money
@@ -68,8 +86,29 @@ class BankAccount {
 		if (amount > this._balance) {
 			throw new Error("Insufficient funds.")
 		}
+		if (amount > this._dailyWithdrawalLimit) {
+			console.log("Exceeds daily withdrawal limit")
+			return
+		}
 		this._balance -= amount
 		this._transactions.push(new Transaction("Withdrawal", amount))
+	}
+
+	// Method to transfer money to another account
+	transfer(amount, recipientAccount) {
+		if (amount > this._balance) {
+			console.log("Insufficient funds")
+			return
+		}
+		if (amount > this._dailyWithdrawalLimit) {
+			console.log("Exceeds daily withdrawal limit")
+			return
+		}
+		this._balance -= amount
+		this._transactions.push(
+			new Transaction("Transfer to " + recipientAccount.accountNumber, amount)
+		)
+		recipientAccount.deposit(amount)
 	}
 
 	// Method to get all transactions
@@ -78,24 +117,20 @@ class BankAccount {
 	}
 }
 
-const account = new BankAccount(123456789, "Aziz", "Akande")
+// Usage example:
+const account1 = new BankAccount(123456789, "Aziz", "Akande", 1000, 600)
+const account2 = new BankAccount(987654321, "Betha", "Sofian", 2000)
 
-console.log("Account number:", account.accountNumber)
-console.log("First name:", account.firstName)
-console.log("Last name:", account.lastName)
+account1.deposit(800)
+account1.withdraw(200)
+account1.withdraw(700)
 
-account.deposit(1000)
-account.withdraw(500)
+account1.transfer(300, account2)
 
-const transactions = account.getTransactions()
-console.log("Account holder:", account.accountHolder)
-console.log("Balance:", account.balance)
-console.log("Transactions:", transactions)
+console.log("Balance for account 1:", account1.balance)
+const transactionsAccount1 = account1.getTransactions()
+console.log("Transactions on Account 1:", transactionsAccount1)
 
-for (const transaction of transactions) {
-	console.log(
-		`Transaction type: ${transaction.type}, Amount: ${transaction.amount}, Timestamp: ${transaction.timestamp}`
-	)
-}
-
-//done
+console.log("Balance for account 2:", account2.balance)
+const transactionsAccount2 = account2.getTransactions()
+console.log("Transactions on Account 2:", transactionsAccount2)
